f7d8e83c5a89fc303a3d1448992ecacf55942ec9
merehan <mirhan.emad@lacasa-egy.com>
2024-09-21 13:17:18 +0300

    cc

--- src/components/CarouselComponent/CarouselComponent.js
+++ src/components/CarouselComponent/CarouselComponent.js
@@ -23,6 +23,13 @@ const CarouselComponent = () => (
       <Carousel.Item>
         <img
           className="d-block w-100 carousel-image"
+          src="./A1.jpg"
+          alt="Image 1"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel-image"
           src="./A2.jpg"
           alt="Image 2"
         />
